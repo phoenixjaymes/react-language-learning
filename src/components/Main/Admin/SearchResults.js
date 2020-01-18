184f9f46f5910bf44b5b571f02b9cdfd5a4d019f
Jaymes Young <jaymes@phoenixjaymes.com>
2020-01-17 16:35:37 -0800

    Added table sorting functions
    Added all table sorting functions
    Rewrote getHead and getRows with useCallback hook

--- src/components/Main/Admin/SearchResults.js
+++ src/components/Main/Admin/SearchResults.js
@@ -1,25 +1,99 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
 
 import SearchConjugation from './SearchConjugation';
 
-const SearchResults = ({ data, sortTable }) => {
+const SearchResults = ({ data }) => {
   const [searchHeader, setSearchHeader] = useState();
   const [searchRows, setSearchRows] = useState();
+  const [sortColumn, setSortColumn] = useState('');
+  const [sortDirection, setSortDirection] = useState('');
 
-  const getHead = (keys) => {
+  const sortAscMemoizedCallback = useCallback((a, b, dataId) => {
+    const nameA = a[dataId].toUpperCase(); // ignore upper and lowercase
+    const nameB = b[dataId].toUpperCase(); // ignore upper and lowercase
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  }, []);
+
+  const sortDscMemoizedCallback = useCallback((a, b, dataId) => {
+    const nameA = a[dataId].toUpperCase(); // ignore upper and lowercase
+    const nameB = b[dataId].toUpperCase(); // ignore upper and lowercase
+
+    if (nameA > nameB) {
+      return -1;
+    }
+    if (nameA < nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  }, []);
+
+  const sortIdMemoizedCallback = useCallback(() => {
+    if (sortColumn !== 'id') {
+      setSortColumn('id');
+      setSortDirection('asc');
+      data.sort((a, b) => a.id - b.id);
+    } else if (sortColumn === 'id' && sortDirection === 'asc') {
+      setSortDirection('dsc');
+      data.sort((a, b) => b.id - a.id);
+    } else if (sortColumn === 'id' && sortDirection === 'dsc') {
+      setSortDirection('asc');
+      data.sort((a, b) => a.id - b.id);
+    }
+  }, [data, sortColumn, sortDirection]);
+
+  const sortColumnsMemoizedCallback = useCallback((dataId) => {
+    if (sortColumn !== dataId) {
+      setSortColumn(dataId);
+      setSortDirection('asc');
+      data.sort((a, b) => sortAscMemoizedCallback(a, b, dataId));
+    } else if (sortColumn === dataId && sortDirection === 'asc') {
+      setSortDirection('dsc');
+      data.sort((a, b) => sortDscMemoizedCallback(a, b, dataId));
+    } else if (sortColumn === dataId && sortDirection === 'dsc') {
+      setSortDirection('asc');
+      data.sort((a, b) => sortAscMemoizedCallback(a, b, dataId));
+    }
+  }, [data, sortColumn, sortDirection, sortAscMemoizedCallback, sortDscMemoizedCallback]);
+
+  const sortTableMemoizedCallback = useCallback((e) => {
+    const dataId = e.target.getAttribute('data-id');
+
+    if (dataId === 'id') {
+      sortIdMemoizedCallback();
+    } else {
+      sortColumnsMemoizedCallback(dataId);
+    }
+  }, [sortIdMemoizedCallback, sortColumnsMemoizedCallback]);
+
+  const headMemoizedCallback = useCallback(() => {
+    const keys = Object.keys(data[0]);
+
     const headers = keys.map((header) => {
       if (header === 'conjugation') {
         return <th key={header}>{header}</th>;
       }
 
-      return <th key={header} data-id={header} onClick={sortTable}>{header}</th>;
+      return <th key={header} data-id={header} onClick={sortTableMemoizedCallback}>{header}</th>;
     });
 
     setSearchHeader(<tr>{headers}</tr>);
-  };
+  }, [data, sortTableMemoizedCallback]);
 
-  const getRows = (keys) => {
+  const rowsMemoizedCallback = useCallback(() => {
+    const keys = Object.keys(data[0]);
+
     const rows = data.map((row) => {
       const cells = keys.map((cell) => {
         if (cell === 'conjugation') {
@@ -44,15 +118,14 @@ const SearchResults = ({ data, sortTable }) => {
     });
 
     setSearchRows(rows);
-  };
+  }, [data]);
 
   useEffect(() => {
     if (data[0] !== undefined) {
-      const keys = Object.keys(data[0]);
-      getHead(keys);
-      getRows(keys);
+      headMemoizedCallback();
+      rowsMemoizedCallback();
     }
-  });
+  }, [data, headMemoizedCallback, rowsMemoizedCallback]);
 
   return (
     <table className="search-results">
@@ -66,7 +139,6 @@ const SearchResults = ({ data, sortTable }) => {
 
 SearchResults.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object),
-  sortTable: PropTypes.func,
 };
 
 export default SearchResults;
