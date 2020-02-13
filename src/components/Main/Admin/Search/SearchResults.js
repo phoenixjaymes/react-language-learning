6afa5a2b85a2e7e77cef2559955696a15e4f8d2d
Jaymes Young <jaymes@phoenixjaymes.com>
2020-02-13 13:29:36 -0800

    Moved files form admin dir

---
+++ src/components/Main/Admin/Search/SearchResults.js
@@ -0,0 +1,146 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import PropTypes from 'prop-types';
+
+import SearchConjugation from './SearchConjugation';
+
+import styles from './searchResults.module.css';
+
+const SearchResults = ({ data }) => {
+  const [searchHeader, setSearchHeader] = useState();
+  const [searchRows, setSearchRows] = useState();
+  const [sortColumn, setSortColumn] = useState('');
+  const [sortDirection, setSortDirection] = useState('');
+
+  const sortAscMemoizedCallback = useCallback((a, b, dataId) => {
+    const nameA = a[dataId].toUpperCase(); // ignore upper and lowercase
+    const nameB = b[dataId].toUpperCase(); // ignore upper and lowercase
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  }, []);
+
+  const sortDscMemoizedCallback = useCallback((a, b, dataId) => {
+    const nameA = a[dataId].toUpperCase(); // ignore upper and lowercase
+    const nameB = b[dataId].toUpperCase(); // ignore upper and lowercase
+
+    if (nameA > nameB) {
+      return -1;
+    }
+    if (nameA < nameB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  }, []);
+
+  const sortIdMemoizedCallback = useCallback(() => {
+    if (sortColumn !== 'id') {
+      setSortColumn('id');
+      setSortDirection('asc');
+      data.sort((a, b) => a.id - b.id);
+    } else if (sortColumn === 'id' && sortDirection === 'asc') {
+      setSortDirection('dsc');
+      data.sort((a, b) => b.id - a.id);
+    } else if (sortColumn === 'id' && sortDirection === 'dsc') {
+      setSortDirection('asc');
+      data.sort((a, b) => a.id - b.id);
+    }
+  }, [data, sortColumn, sortDirection]);
+
+  const sortColumnsMemoizedCallback = useCallback((dataId) => {
+    if (sortColumn !== dataId) {
+      setSortColumn(dataId);
+      setSortDirection('asc');
+      data.sort((a, b) => sortAscMemoizedCallback(a, b, dataId));
+    } else if (sortColumn === dataId && sortDirection === 'asc') {
+      setSortDirection('dsc');
+      data.sort((a, b) => sortDscMemoizedCallback(a, b, dataId));
+    } else if (sortColumn === dataId && sortDirection === 'dsc') {
+      setSortDirection('asc');
+      data.sort((a, b) => sortAscMemoizedCallback(a, b, dataId));
+    }
+  }, [data, sortColumn, sortDirection, sortAscMemoizedCallback, sortDscMemoizedCallback]);
+
+  const sortTableMemoizedCallback = useCallback((e) => {
+    const dataId = e.target.getAttribute('data-id');
+
+    if (dataId === 'id') {
+      sortIdMemoizedCallback();
+    } else {
+      sortColumnsMemoizedCallback(dataId);
+    }
+  }, [sortIdMemoizedCallback, sortColumnsMemoizedCallback]);
+
+  const headMemoizedCallback = useCallback(() => {
+    const keys = Object.keys(data[0]);
+
+    const headers = keys.map((header) => {
+      if (header === 'conjugation') {
+        return <th key={header}>{header}</th>;
+      }
+
+      return <th key={header} data-id={header} onClick={sortTableMemoizedCallback}>{header}</th>;
+    });
+
+    setSearchHeader(<tr>{headers}</tr>);
+  }, [data, sortTableMemoizedCallback]);
+
+  const rowsMemoizedCallback = useCallback(() => {
+    const keys = Object.keys(data[0]);
+
+    const rows = data.map((row) => {
+      const cells = keys.map((cell) => {
+        if (cell === 'conjugation') {
+          return (
+            <td key="conjugation" className={styles.searchResultsConjugation}>
+              View
+              <SearchConjugation data={row[cell]} />
+            </td>
+          );
+        }
+
+        return (
+          <td key={cell}>{row[cell]}</td>
+        );
+      });
+
+      return (
+        <tr key={row.id}>
+          {cells}
+        </tr>
+      );
+    });
+
+    setSearchRows(rows);
+  }, [data]);
+
+  useEffect(() => {
+    if (data[0] !== undefined) {
+      headMemoizedCallback();
+      rowsMemoizedCallback();
+    }
+  }, [data, headMemoizedCallback, rowsMemoizedCallback]);
+
+  return (
+    <table className={styles.searchResults}>
+      <tbody>
+        {searchHeader}
+        {searchRows}
+      </tbody>
+    </table>
+  );
+};
+
+SearchResults.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default SearchResults;
---
+++ src/components/Main/Admin/Search/index.js
@@ -0,0 +1,142 @@
+import React, { Component } from 'react';
+import { LearningContext } from '../../../Context';
+
+import SearchResults from './SearchResults';
+
+import svgSearch from './search.svg';
+import styles from './search.module.css';
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      jsonData: [],
+      modifiedData: [],
+      itemType: '',
+      itemRange: 'a-d',
+      extraOptions: [],
+    };
+  }
+
+  getExtraOptions = (itemType) => {
+    const value = this.context;
+    const { lang, categories } = value;
+    // const options = categories[lang][itemType].map((item) => (
+    //   <option key={item.id} value={item.id}>{item.name}</option>
+    // ));
+
+    this.setState({ extraOptions: categories[lang][itemType] });
+  }
+
+  handleType = (e) => {
+    if (e.target.value === 'adjective'
+    || e.target.value === 'noun'
+    || e.target.value === 'verb') {
+      this.getExtraOptions(e.target.value);
+      this.setState({ itemType: e.target.value });
+    } else {
+      this.setState({
+        itemType: e.target.value,
+        extraOptions: [],
+      });
+    }
+  }
+
+  handleRange = (e) => this.setState({ itemRange: e.target.value })
+
+  handleSearchClick = () => {
+    const { itemType, itemRange } = this.state;
+    const { lang } = this.context;
+
+    if (itemType === '') {
+      return;
+    }
+
+    fetch(`http://phoenixjaymes.com/assets/data/language/get-search.php?lang=${lang}&type=${itemType}&range=${itemRange}`)
+      .then((reponse) => reponse.json())
+      .then(
+        (responseData) => {
+          this.setState({
+            jsonData: responseData.data,
+            modifiedData: responseData.data,
+          });
+        },
+      )
+      .catch((error) => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
+  render() {
+    const {
+      jsonData, modifiedData, itemType, itemRange, extraOptions,
+    } = this.state;
+    let results;
+
+    if (jsonData.status === 'fail') {
+      results = <h3>No results were returned</h3>;
+    } else {
+      results = <SearchResults data={modifiedData} sortTable={this.sortTable} />;
+    }
+
+    return (
+      <div>
+        <form className={styles.searchForm} autoComplete="off" onSubmit={this.handleSubmit}>
+
+          <label className={styles.searchFormLabel} htmlFor="selType">
+            Search Type
+            <select
+              id="selType"
+              name="selType"
+              value={itemType}
+              onChange={this.handleType}
+            >
+              <option value="">Select Type</option>
+              <option value="adjective">Adjective</option>
+              <option value="category">Category</option>
+              <option value="noun">Noun</option>
+              <option value="phrase">Phrase</option>
+              <option value="sentence">Sentence</option>
+              <option value="verb">Verb</option>
+            </select>
+          </label>
+
+          <label className={styles.searchFormLabel} htmlFor="selRange">
+            Range
+            <select
+              id="selRange"
+              name="selRange"
+              value={itemRange}
+              onChange={this.handleRange}
+            >
+              <option value="a-d">A - D</option>
+              <option value="e-h">E - H</option>
+              <option value="i-l">I - L</option>
+              <option value="m-p">M - P</option>
+              <option value="q-t">Q - T</option>
+              <option value="u-z">U - Z</option>
+              <option value="umlauts">Umlauts</option>
+              {extraOptions.length > 0 && (
+                extraOptions.map((item) => (
+                  <option key={item.id} value={item.id}>{item.name}</option>
+                ))
+              )}
+            </select>
+          </label>
+          <div className={styles.searchFormBtnSearch}>
+            <span onClick={this.handleSearchClick} role="button" onKeyPress={this.handleSearchClick}>
+              <img src={svgSearch} alt="search" />
+            </span>
+          </div>
+        </form>
+
+        {results}
+
+      </div>
+    );
+  }
+}
+
+Search.contextType = LearningContext;
+
+export default Search;
