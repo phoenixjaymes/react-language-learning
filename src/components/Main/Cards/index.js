d213146efb63e6aa301e536b92a9807d66dca4f4
Jaymes Young <jaymes@phoenixjaymes.com>
2020-02-14 13:12:09 -0800

    Removed ChangeCategoryButton component
    Removed component and made it it's own file.
    Imported file.

--- src/components/Main/Cards/index.js
+++ src/components/Main/Cards/index.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { LearningContext } from '../../Context';
 
+import ChangeCategoryButton from './CardsChangeCategoryButton';
+
 const CardOptionsLinks = ({ category, catagoryList, lang }) => {
   const buttons = catagoryList.map(option => (
     <Link key={option.id} to={`/${lang}/cards/${category}/${option.id}`} className="activity_buttons__btn">
@@ -21,25 +23,6 @@ CardOptionsLinks.propTypes = {
   catagoryList: PropTypes.arrayOf(PropTypes.object),
   category: PropTypes.string,
   lang: PropTypes.string,
-};
-
-const ChangeCategoryButton = ({ category, changeCategory }) => {
-  const categoryUCase = category.charAt(0).toUpperCase() + category.substring(1);
-  return (
-    <button
-      type="button"
-      className="activity_buttons__btn"
-      onClick={() => changeCategory(category)}
-      onKeyPress={() => changeCategory(category)}
-    >
-      {categoryUCase}
-    </button>
-  );
-};
-
-ChangeCategoryButton.propTypes = {
-  category: PropTypes.string,
-  changeCategory: PropTypes.func,
 };
 
 // Cards Component
