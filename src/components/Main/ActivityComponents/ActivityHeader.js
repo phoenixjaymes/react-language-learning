b3c4daa674c030b579155e89346ed80e1c88211a
Jaymes Young <jaymes@phoenixjaymes.com>
2020-02-14 12:17:02 -0800

    Imported style module
    Imported style module and changed classnames

--- src/components/Main/ActivityComponents/ActivityHeader.js
+++ src/components/Main/ActivityComponents/ActivityHeader.js
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
+import styles from './activityHeader.module.css';
+
 const ActivityHeader = ({ page, heading }) => (
-  <header className="activity__header">
-    <Link to={`/${page}`} className="btn-activitiy activity__close" ng-click="showWords = false; finalMessage = false">X</Link>
-    <h2 className="activity__heading">{heading}</h2>
+  <header className={styles.header}>
+    <Link to={`/${page}`} className="btn-activitiy activity__close">X</Link>
+    <h2 className={styles.heading}>{heading}</h2>
   </header>
 );
 
