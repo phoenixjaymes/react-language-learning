2f9bb1e8f685d754753ddaf36bc56d3f88394020
Jaymes Young <jaymes@phoenixjaymes.com>
2020-02-17 14:07:59 -0800

    Imported style module
    Imported style module and change classNames

--- src/components/Main/Cards/CardControls.js
+++ src/components/Main/Cards/CardControls.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import styles from './cardControls.module.css';
+
 const CardControls = ({
   currentCard, totalCards, cardBack, cardNext,
 }) => (
-  <div className="cards-controls">
-    <button type="button" href="#back" className="cards-controls__btn" onClick={cardBack}>&lArr;</button>
-    <span className="cards-controls__count">
+  <div className={styles.cardsControls}>
+    <button type="button" href="#back" className={styles.button} onClick={cardBack}>&lArr;</button>
+    <span className={styles.cardsCount}>
       {`${currentCard} of ${totalCards}`}
     </span>
-    <button type="button" href="#next" className="cards-controls__btn" onClick={cardNext}>&rArr;</button>
+    <button type="button" href="#next" className={styles.button} onClick={cardNext}>&rArr;</button>
   </div>
 );
 
