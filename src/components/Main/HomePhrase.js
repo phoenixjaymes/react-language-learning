f2b338f5ebac21649a7f59b5f3893027d77f87a2
Jaymes Young <jaymes@phoenixjaymes.com>
2020-03-05 16:14:03 -0800

    Initial commit

---
+++ src/components/Main/HomePhrase.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './homePhrase.module.css';
+
+const HomePhrase = () => {
+  const phrase = {
+    english: 'I will keep an eye on you.',
+    translation: 'Ich werde dich im Auge behalten.',
+  };
+
+  return (
+    <div className={styles.phrase}>
+      <h2>Daily Phrase</h2>
+      <h3 className={styles.phraseTranslation}>{phrase.translation}</h3>
+    </div>
+  );
+}
+
+export default HomePhrase;
