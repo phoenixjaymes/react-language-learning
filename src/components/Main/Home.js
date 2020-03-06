4a3bc0ca8b7b09ee2e08d3ae5851280739061516
Jaymes Young <jaymes@phoenixjaymes.com>
2020-03-05 16:14:47 -0800

    Added HomePhrase component

--- src/components/Main/Home.js
+++ src/components/Main/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import HomeImage from './HomeImage';
+import HomePhrase from './HomePhrase';
 
 const Home = () => (
   <section className="home">
     <h1 className="home__title">Language Learning</h1>
     <HomeImage />
+    <HomePhrase />
   </section>
 );
 
