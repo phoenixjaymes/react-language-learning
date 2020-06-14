c5b064dbf8ccd476d0dc9d89cc29faf2a059926a
Jaymes Young <jaymes@phoenixjaymes.com>
2020-06-14 14:58:42 -0700

    Removed state
    Removed state
    Removed ChangeCategoryButton and import
    Removevd changeCategory func and replaced with components
    Imported CardCategories components

--- src/components/Main/Cards/index.js
+++ src/components/Main/Cards/index.js
@@ -2,75 +2,57 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { LearningContext } from '../../Context';
 
-import ChangeCategoryButton from './CardsChangeCategoryButton';
 import ActivityLink from '../ActivityComponents/ActivityLink';
+import CardCategoriesGeneric from './CardCategoriesGeneric';
+import CardCategoriesVerb from './CardCategoriesVerb';
 
 // Cards Component
 class Cards extends Component {
-  state = {
-    category: 'adjective',
-    catagoryList: [],
-  }
-
   componentDidMount() {
     const value = this.context;
     const { setDocumentTitle } = value.actions;
-
     setDocumentTitle('Flash Cards');
   }
 
-  changeCategory = (categoryName) => {
-    const { match } = this.props;
-    const { lang } = match.params;
-    const value = this.context;
-
-    this.setState({
-      category: categoryName,
-      catagoryList: value.categories[lang][categoryName],
-    });
-  }
-
   render() {
-    const { lang, labels } = this.context;
-    const { category, catagoryList } = this.state;
+    const { lang, categories, labels } = this.context;
     const headingLabel = labels.us.languages[lang];
-
-    const buttons = catagoryList.map(option => (
-      <ActivityLink
-        key={option.id}
-        toPath={`/${lang}/cards/${category}/${option.id}`}
-        text={option.name}
-      />
-    ));
 
     return (
       <section className="activity-section">
         <h1>{`${headingLabel} Cards`}</h1>
 
-        <h3>Select your Cards</h3>
+        <div className="activity-buttons activity-categories">
+          <h2>Adjectives</h2>
+          <CardCategoriesGeneric
+            lang={lang}
+            category="adjective"
+            categoryList={categories[lang].adjective}
+          />
 
-        <div className="activity-buttons">
-          <ChangeCategoryButton category="adjective" changeCategory={this.changeCategory} />
-          <br />
-          <ChangeCategoryButton category="noun" changeCategory={this.changeCategory} />
+          <h2>Nouns</h2>
           {lang !== 'za' && (
-            <ChangeCategoryButton category="gender" changeCategory={this.changeCategory} />
+            <CardCategoriesGeneric
+              lang={lang}
+              category="gender"
+              categoryList={categories[lang].gender}
+            />
           )}
-          <br />
-          <ChangeCategoryButton category="verb" changeCategory={this.changeCategory} />
-          <br />
+
+          <CardCategoriesGeneric
+            lang={lang}
+            category="noun"
+            categoryList={categories[lang].noun}
+          />
+
+          <h2>Verbs</h2>
+          <CardCategoriesVerb lang={lang} />
+
+          <h2>Phrases</h2>
           <ActivityLink
             toPath={`/${lang}/cards/phrase`}
             text="Phrases"
           />
-          <ActivityLink
-            toPath={`/${lang}/cards/study`}
-            text="Study"
-          />
-        </div>
-
-        <div className="activity-buttons activity-categories">
-          {buttons}
         </div>
       </section>
     );
