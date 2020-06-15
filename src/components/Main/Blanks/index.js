691fcf7b108ab138d6b38fbca5d1a3391a1f74e8
Jaymes Young <jaymes@phoenixjaymes.com>
2020-06-14 17:22:19 -0700

    Added component
    Added ActivityCategoriesGeneric component and removed ActivityLinks

--- src/components/Main/Blanks/index.js
+++ src/components/Main/Blanks/index.js
@@ -2,24 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import withContext from '../../Context';
-import ActivityLink from '../ActivityComponents/ActivityLink';
+import ActivityCategoriesGeneric from '../ActivityComponents/ActivityCategoriesGeneric';
 
 const Blanks = ({ context }) => {
   const { categories, lang, labels } = context;
   const headingLabel = labels.us.languages[lang];
 
-  const buttons = categories[lang].blank.map(option => (
-    <ActivityLink key={option.id} toPath={`/${lang}/blanks/${option.id}`} text={option.name} />
-  ));
-
   return (
     <section className="activity-section">
       <h1>{`${headingLabel} Blanks`}</h1>
 
-      <h3>Select your Blanks</h3>
-
       <div className="activity-buttons activity-categories">
-        {buttons}
+        <ActivityCategoriesGeneric
+          lang={lang}
+          activity="blanks"
+          category=""
+          categoryList={categories[lang].blank}
+        />
       </div>
     </section>
   );
