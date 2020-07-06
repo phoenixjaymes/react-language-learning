2574b6112ad2eeae2d8ad36f31be946b0712ea8a
Jaymes Young <jaymes@phoenixjaymes.com>
2020-07-05 17:09:38 -0700

    Removed SentenceOptionsLink func
    Removed all lang variables and references

--- src/components/Main/Sentences/index.js
+++ src/components/Main/Sentences/index.js
@@ -1,26 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { LearningConsumer } from '../../Context';
 
 import ActivityLink from '../ActivityComponents/ActivityLink';
 import ActivityCategoriesGeneric from '../ActivityComponents/ActivityCategoriesGeneric';
-
-const SentenceOptionsLinks = ({ lang, categories }) => {
-  const buttons = categories.map((option) => (
-    <ActivityLink key={option.id} toPath={`/${lang}/sentences/category/${option.id}`} text={option.name} />
-  ));
-
-  return (
-    <div className="activity-buttons activity-categories">
-      {buttons}
-    </div>
-  );
-};
-
-SentenceOptionsLinks.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.object).isRequired,
-  lang: PropTypes.string.isRequired,
-};
 
 const Sentences = () => (
   <LearningConsumer>
@@ -34,36 +16,27 @@ const Sentences = () => (
 
             <div className="activity-buttons activity-categories">
               <ActivityLink
-                toPath={`/${lang}/sentences/type/random`}
+                toPath="/sentences/type/random"
                 text="Random"
               />
               <br />
 
               <ActivityCategoriesGeneric
-                lang={lang}
                 activity="sentences"
                 category="type"
                 categoryList={categories[lang].sentence_type}
               />
 
               <ActivityCategoriesGeneric
-                lang={lang}
                 activity="sentences"
                 category="categpry"
                 categoryList={categories[lang].sentence}
               />
-
-
-              {/* {categories.de !== undefined && (
-                <SentenceOptionsLinks
-                  lang={lang}
-                  categories={categories[lang].sentence}
-                />
-              )} */}
             </div>
           </section>
         );
-      }}
+      }
+    }
   </LearningConsumer>
 );
 
