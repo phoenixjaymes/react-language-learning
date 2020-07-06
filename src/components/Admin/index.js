e85a4df269a5e10fe0ad1f807e1cdabd94f32a57
Jaymes Young <jaymes@phoenixjaymes.com>
2020-07-06 13:18:59 -0700

    Refactored code
    Removed prop-types
    Added useContext
    Combined ListItem conponent and getLinks func into getListItem func.
    Replaced ListItem components with getListItem func

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   Route,
   Redirect,
   Link,
 } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import { LearningConsumer } from '../Context';
+
+import { LearningContext } from '../Context';
 
 import AdminContent from './AdminContent';
 import Search from './Search';
@@ -19,21 +19,10 @@ import svgDelete from './trash.svg';
 import svgSearch from './Search/search.svg';
 import svgRefresh from './refresh.svg';
 
-
-const ListItem = ({ heading, links }) => (
-  <li className="options-menu__item">
-    <h3>{heading}</h3>
-    {links}
-  </li>
-);
-
-ListItem.propTypes = {
-  heading: PropTypes.string,
-  links: PropTypes.arrayOf(PropTypes.object),
-};
-
 const Admin = () => {
-  const getLinks = (category) => {
+  const { isLoggedIn, actions } = useContext(LearningContext);
+
+  const getListItem = (category, heading) => {
     const functions = [
       { type: 'add', img: svgAdd },
       { type: 'update', img: svgUpdate },
@@ -49,104 +38,92 @@ const Admin = () => {
       );
     });
 
-    return links;
+    return (
+      <li className="options-menu__item">
+        <h3>{heading}</h3>
+        {links}
+      </li>
+    );
   };
 
   return (
-    <LearningConsumer>
-      {
-        (context) => {
-          const { isLoggedIn, actions } = context;
-          const adjectiveLinks = getLinks('adjective');
-          const blankLinks = getLinks('blank');
-          const categoryLinks = getLinks('category');
-          const nounLinks = getLinks('noun');
-          const phraseLinks = getLinks('phrase');
-          const sentenceLinks = getLinks('sentence');
-          const verbLinks = getLinks('verb');
+    <div className="admin">
+      <nav>
+        <h3>Admin</h3>
+        <ul className="options-menu">
+          <li className="options-menu__item">
+            <h3>Tools</h3>
+            <span
+              className="faux-btn"
+              role="button"
+              onClick={() => actions.refreshCategories()}
+              onKeyPress={() => actions.refreshCategories()}
+              tabIndex="0"
+            >
+              <img src={svgRefresh} alt="Refresh Categories" />
+            </span>
+            <Link to="/admin/search">
+              <img src={svgSearch} alt="search" />
+            </Link>
+          </li>
 
-          return (
-            <div className="admin">
-              <nav>
-                <h3>Admin</h3>
-                <ul className="options-menu">
-                  <li className="options-menu__item">
-                    <h3>Tools</h3>
-                    <span
-                      className="faux-btn"
-                      role="button"
-                      onClick={() => actions.refreshCategories()}
-                      onKeyPress={() => actions.refreshCategories()}
-                      tabIndex="0"
-                    >
-                      <img src={svgRefresh} alt="Refresh Categories" />
-                    </span>
-                    <Link to="/admin/search">
-                      <img src={svgSearch} alt="search" />
-                    </Link>
-                  </li>
+          {getListItem('adjective', 'Adjectives')}
+          {getListItem('blank', 'Blanks')}
+          {getListItem('category', 'Categories')}
+          {getListItem('noun', 'Nouns')}
+          {getListItem('phrase', 'Phrases')}
+          {getListItem('sentence', 'Sentences')}
+          {getListItem('verb', 'Verbs')}
 
-                  <ListItem heading="Adjectives" links={adjectiveLinks} />
-                  <ListItem heading="Blanks" links={blankLinks} />
-                  <ListItem heading="Categories" links={categoryLinks} />
-                  <ListItem heading="Nouns" links={nounLinks} />
-                  <ListItem heading="Phrases" links={phraseLinks} />
-                  <ListItem heading="Sentences" links={sentenceLinks} />
-                  <ListItem heading="Verbs" links={verbLinks} />
+          <li className="options-menu__item">
+            <h3>
+              Verbs
+              <span>(perfect)</span>
+            </h3>
+            <Link to="/admin/perfect/update">
+              <img src={svgUpdate} alt="update icon" />
+            </Link>
+          </li>
 
-                  <li className="options-menu__item">
-                    <h3>
-                      Verbs
-                      <span>(perfect)</span>
-                    </h3>
-                    <Link to="/admin/perfect/update">
-                      <img src={svgUpdate} alt="update icon" />
-                    </Link>
-                  </li>
+          <li className="options-menu__item">
+            <h3>
+              Verbs
+              <span>(imperfect)</span>
+            </h3>
+            <Link to="/admin/imperfect/update">
+              <img src={svgUpdate} alt="update icon" />
+            </Link>
+          </li>
 
-                  <li className="options-menu__item">
-                    <h3>
-                      Verbs
-                      <span>(imperfect)</span>
-                    </h3>
-                    <Link to="/admin/imperfect/update">
-                      <img src={svgUpdate} alt="update icon" />
-                    </Link>
-                  </li>
+          <hr />
 
-                  <hr />
+          <li className="options-menu__item"><Link to="/" onClick={() => actions.setLogin(false)}>Logout</Link></li>
+        </ul>
+      </nav>
 
-                  <li className="options-menu__item"><Link to="/" onClick={() => actions.setLogin(false)}>Logout</Link></li>
-                </ul>
-              </nav>
-
-              <section>
-                <Route
-                  path="/admin/search"
-                  render={({ match }) => (
-                    !isLoggedIn ? (
-                      <Redirect to="/" />
-                    ) : (
-                        <Search match={match} />
-                      )
-                  )}
-                />
-                <Route
-                  path="/admin/:category/:modifyType"
-                  render={({ match }) => (
-                    !isLoggedIn ? (
-                      <Redirect to="/" />
-                    ) : (
-                        <AdminContent match={match} />
-                      )
-                  )}
-                />
-              </section>
-            </div>
-          );
-        }
-      }
-    </LearningConsumer>
+      <section>
+        <Route
+          path="/admin/search"
+          render={({ match }) => (
+            !isLoggedIn ? (
+              <Redirect to="/" />
+            ) : (
+                <Search match={match} />
+              )
+          )}
+        />
+        <Route
+          path="/admin/:category/:modifyType"
+          render={({ match }) => (
+            !isLoggedIn ? (
+              <Redirect to="/" />
+            ) : (
+                <AdminContent match={match} />
+              )
+          )}
+        />
+      </section>
+    </div>
   );
 };
 
