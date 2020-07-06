d33b0e00709a469a43513333d04a00ab37073d8d
Jaymes Young <jaymes@phoenixjaymes.com>
2020-07-06 13:59:40 -0700

    Refactored code
    Added useContext

--- src/components/RoutesComponent.js
+++ src/components/RoutesComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {
   Route,
   Redirect,
@@ -21,62 +21,59 @@ import Admin from './Admin';
 import NotFound from './NotFound';
 import LoginForm from './Admin/LoginForm';
 
-const Main = () => (
-  <main>
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
+const Main = () => {
+  const { isLoggedIn } = useContext(LearningConsumer);
 
-      <Route path="/cards/:category/:id?" render={({ match }) => <CardsActivity match={match} />} />
-      <Route path="/cards" render={({ match }) => <Cards match={match} />} />
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-      <Route path="/words/:category/:id?" render={({ match }) => <WordsActivity match={match} />} />
-      <Route path="/words">
-        <Words />
-      </Route>
+        <Route path="/cards/:category/:id?" render={({ match }) => <CardsActivity match={match} />} />
+        <Route path="/cards" render={({ match }) => <Cards match={match} />} />
 
-      <Route path="/sentences/:category/:id?" render={({ match }) => <SentencesActivity match={match} />} />
-      <Route path="/sentences">
-        <Sentences />
-      </Route>
+        <Route path="/words/:category/:id?" render={({ match }) => <WordsActivity match={match} />} />
+        <Route path="/words">
+          <Words />
+        </Route>
 
-      <Route path="/blanks/:category/:id?" render={({ match }) => <BlanksActivity match={match} />} />
-      <Route path="/blanks">
-        <Blanks />
-      </Route>
+        <Route path="/sentences/:category/:id?" render={({ match }) => <SentencesActivity match={match} />} />
+        <Route path="/sentences">
+          <Sentences />
+        </Route>
 
-      <Route
-        path="/grammar"
-        render={({ match, location }) => (
-          <Grammar
-            match={match}
-            location={location}
-          />
-        )}
-      />
+        <Route path="/blanks/:category/:id?" render={({ match }) => <BlanksActivity match={match} />} />
+        <Route path="/blanks">
+          <Blanks />
+        </Route>
 
-      <Route path="/login" render={() => <LoginForm />} />
-      <LearningConsumer>
-        {(context) => (
-          <Route
-            path="/admin"
-            render={() => (
-              !context.isLoggedIn ? (
-                <Redirect to="/" />
-              ) : (
-                  <Admin />
-                )
-            )}
-          />
-        )}
-      </LearningConsumer>
+        <Route
+          path="/grammar"
+          render={({ match, location }) => (
+            <Grammar
+              match={match}
+              location={location}
+            />
+          )}
+        />
 
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
-  </main>
-);
+        <Route path="/login">
+          <LoginForm />
+        </Route>
+
+        <Route
+          path="/admin"
+          render={() => (!isLoggedIn ? <Redirect to="/" /> : <Admin />)}
+        />
+
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </main>
+  );
+};
 
 export default Main;
