6d851f4ac754a5cd3c818188029098e1be4c5ec4
Jaymes Young <jaymes@phoenixjaymes.com>
2020-07-07 15:05:28 -0700

    Removed lang prop

--- src/components/Main/FinalMessage.js
+++ src/components/Main/FinalMessage.js
@@ -2,21 +2,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-const FinalMessage = ({
-  type, lang,
-}) => {
+const FinalMessage = ({ type }) => {
   const message = '';
 
   return (
     <div className="final-message start-ani">
       <div className="final-message__wrap">
-        <h3 className="sentences__message-text">{`${message}`}<br />Great Job</h3>
+        <h3 className="sentences__message-text">
+          {`${message}`}
+          <br />
+          Great Job
+        </h3>
         <p className="centered">Would you like to continue?</p>
 
         <br />
 
         <div className="activity-buttons">
-          <Link to={`/${lang}/${type}`} className="activity_buttons__btn">Continue</Link>
+          <Link to={`/${type}`} className="activity_buttons__btn">Continue</Link>
         </div>
       </div>
     </div>
@@ -25,7 +27,6 @@ const FinalMessage = ({
 
 FinalMessage.propTypes = {
   type: PropTypes.string,
-  lang: PropTypes.string,
 };
 
 export default FinalMessage;
