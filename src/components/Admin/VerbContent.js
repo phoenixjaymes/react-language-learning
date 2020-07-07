69101b3627235a83c9a821ef6814b16fea552957
Jaymes Young <jaymes@phoenixjaymes.com>
2020-07-07 14:44:52 -0700

    Refactored code
    Added useContext
    Removed LearningConsumer component

--- src/components/Admin/VerbContent.js
+++ src/components/Admin/VerbContent.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { LearningConsumer } from '../Context';
+import { LearningContext } from '../Context';
 
 import Verb from './Verb';
 import VerbNl from './VerbNl';
@@ -14,56 +14,48 @@ import VerbImperfectNl from './VerbImperfectNl';
 import VerbImperfectZA from './VerbImperfectZA';
 
 const VerbContent = ({ cat, modifyType }) => {
-  return (
-    <LearningConsumer>
-      {
-        (context) => {
-          const { lang } = context;
+  const { lang } = useContext(LearningContext);
 
-          if (cat === 'verb' && modifyType === 'delete') {
-            return <VerbDelete />;
-          }
+  if (cat === 'verb' && modifyType === 'delete') {
+    return <VerbDelete />;
+  }
 
-          if (cat === 'verb') {
-            if (lang === 'nl') {
-              return <VerbNl categoryName={cat} modifyType={modifyType} />;
-            }
+  if (cat === 'verb') {
+    if (lang === 'nl') {
+      return <VerbNl categoryName={cat} modifyType={modifyType} />;
+    }
 
-            if (lang === 'af') {
-              return <VerbZA categoryName={cat} modifyType={modifyType} />;
-            }
+    if (lang === 'af') {
+      return <VerbZA categoryName={cat} modifyType={modifyType} />;
+    }
 
-            return <Verb categoryName={cat} modifyType={modifyType} />;
-          }
+    return <Verb categoryName={cat} modifyType={modifyType} />;
+  }
 
-          if (cat === 'perfect') {
-            if (lang === 'nl') {
-              return <VerbPerfectNl categoryName={cat} modifyType={modifyType} />;
-            }
+  if (cat === 'perfect') {
+    if (lang === 'nl') {
+      return <VerbPerfectNl categoryName={cat} modifyType={modifyType} />;
+    }
 
-            if (lang === 'af') {
-              return <VerbPerfectZA categoryName={cat} modifyType={modifyType} />;
-            }
+    if (lang === 'af') {
+      return <VerbPerfectZA categoryName={cat} modifyType={modifyType} />;
+    }
 
-            return <VerbPerfect categoryName={cat} modifyType={modifyType} />;
-          }
+    return <VerbPerfect categoryName={cat} modifyType={modifyType} />;
+  }
 
-          if (cat === 'imperfect') {
-            if (lang === 'nl') {
-              return <VerbImperfectNl categoryName={cat} modifyType={modifyType} />;
-            }
+  if (cat === 'imperfect') {
+    if (lang === 'nl') {
+      return <VerbImperfectNl categoryName={cat} modifyType={modifyType} />;
+    }
 
-            if (lang === 'af') {
-              return <VerbImperfectZA categoryName={cat} modifyType={modifyType} />;
-            }
+    if (lang === 'af') {
+      return <VerbImperfectZA categoryName={cat} modifyType={modifyType} />;
+    }
 
-            return <VerbImperfect categoryName={cat} modifyType={modifyType} />;
-          }
-          return null;
-        }
-      }
-    </LearningConsumer>
-  );
+    return <VerbImperfect categoryName={cat} modifyType={modifyType} />;
+  }
+  return null;
 };
 
 VerbContent.propTypes = {
