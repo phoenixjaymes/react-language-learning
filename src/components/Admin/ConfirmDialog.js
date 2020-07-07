ae7fa3582ff769ccf5e9cfec1d06c22c800169e4
Jaymes Young <jaymes@phoenixjaymes.com>
2020-07-07 15:25:50 -0700

    Imported styles from confirmDialog

--- src/components/Admin/ConfirmDialog.js
+++ src/components/Admin/ConfirmDialog.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './confirm-dialog.css';
+import styles from './confirmDialog.module.css';
 
 const ConfirmDialog = ({ dialogMessage, handleYesClick, handleCancelClick }) => (
   <div className="confirm-dialog">
