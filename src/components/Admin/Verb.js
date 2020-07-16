69820e604b2fc476fec0eb82c58a2107b843a9a8
Jaymes Young <jaymes@phoenixjaymes.com>
2020-07-16 13:37:40 -0700

    Added useCallback hook

--- src/components/Admin/Verb.js
+++ src/components/Admin/Verb.js
@@ -1,5 +1,5 @@
 import React, {
-  useState, useContext, useReducer, useEffect,
+  useState, useContext, useReducer, useEffect, useCallback,
 } from 'react';
 import PropTypes from 'prop-types';
 import { LearningContext } from '../Context';
