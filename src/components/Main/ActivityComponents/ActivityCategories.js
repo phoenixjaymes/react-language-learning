2605a6b921db08376d30f3451142e57d5cbf9aeb
Jaymes Young <jaymes@phoenixjaymes.com>
2021-02-20 17:23:37 -0800

    Initial commit

---
+++ src/components/Main/ActivityComponents/ActivityCategories.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import ActivityLink from './ActivityLink';
+
+const ActvityCategories = ({
+  activity, category, categoryList,
+}) => {
+  const wrap = {
+    textAlign: 'center',
+    marginBottom: '15px',
+  };
+
+  let buttons;
+
+  if (activity === 'cards' && category === 'verb') {
+    const verbCategoryList = ['present', 'perfect', 'imperfect', 'dative', 'reflexive', 'separable'];
+    buttons = verbCategoryList.map((option) => (
+      <ActivityLink
+        key={option}
+        toPath={`/cards/verb/${option}`}
+        text={option}
+      />
+    ));
+  } else if (category === '') {
+    buttons = categoryList.map((option) => (
+      <ActivityLink
+        key={option.id}
+        toPath={`/${activity}/${option.id}`}
+        text={option.name}
+      />
+    ));
+  } else {
+    buttons = categoryList.map((option) => (
+      <ActivityLink
+        key={option.id}
+        toPath={`/${activity}/${category}/${option.id}`}
+        text={option.name}
+      />
+    ));
+  }
+
+  return (
+    <div style={wrap}>
+      {buttons}
+    </div>
+  );
+};
+
+ActvityCategories.defaultProps = {
+  categoryList: '',
+};
+
+ActvityCategories.propTypes = {
+  activity: PropTypes.string,
+  category: PropTypes.string,
+  categoryList: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default ActvityCategories;
